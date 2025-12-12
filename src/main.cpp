df668ac2a460c0d93352d7392b960f6c3422f09e
Alina824 <57951927+Alina824@users.noreply.github.com>
2025-12-12 14:54:53 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,11 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    
     app.setApplicationName("Аудио Плеер");
     app.setOrganizationName("АудиоПлеер");
     
     MainWindow window;
     window.show();
-    
     return app.exec();
 }
 
